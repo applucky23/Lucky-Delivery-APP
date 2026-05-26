28d918094714241014d9b324090f778c05fe4478
Sofoniyas <sofi123man@gmail.com>
2026-05-27 00:46:24 +0300

    chore: register notifications urls in main urls

--- apps/backend/config/urls.py
+++ apps/backend/config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('customers.urls')),
     path('api/v1/', include('tasks.urls')),
+    path('api/v1/', include('notifications.urls')),
 ]
